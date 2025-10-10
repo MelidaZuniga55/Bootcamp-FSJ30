89aefb1317eaafb6b38da52069f3f643331f8fcb
MelidaZuniga55 <melidazuniga55@outlook.com>
2025-10-10 12:50:25 -0600

    S-SOLID

---
+++ PHP/SOLID/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*
+    Pilares -> algo que no puede faltar, una base
+    Ej: Agua, dormir, comer, pupusas. oxigeno, los padres la familia, socializar, programar, ocio.
+
+    Principios ? normas de pensamiento y de comportamiento universales, valores, 
+    pueden cambiar dependiendo la percepcion del individuo
+
+    Ej: bañarse, respetuoso con el projimo, cooperativo, honestidad, justicia, responsable
+    empatia, orden, respeto por uno mismo, Igualdad, Solidaridad, etica.
+
+
+
+*/
+
+?>
